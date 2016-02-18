f6bd524018ad0f76dd78155f63c9d5154490db0a
Sylvebois <thomasghaye@hotmail.com>
2016-02-18 17:06:41 +0100

    Suppression du svg (création depuis JS = mauvaises perf) - Sera remplacé par un fichier svg en background-image

--- js/main.js
+++ js/main.js
@@ -1,28 +1,32 @@
 // Scrollspy fluide
 $(function () {   
-    createSVG();
     displayStart();
     applyResize();
     scrollSpy();
     modMailTo();
 });
 
-/*
- * Création du background
- */
-function createSVG() {
-    var svgNS = "http://www.w3.org/2000/svg";
-    
-    var sWidth = $('#start').width();
-    var sHeight = $('#start').height()- $('header nav').height();
 
+/*
+ * Dimensionnement de l'accueil
+ */
+function displayStart() {
+    var startHeight = $(window).height() - $('header nav').height();
     
-    var poly = document.createElementNS(svgNS,'polyligne');
-    poly.setAttributeNS(null, 'points', '5,5 45,45 5,45 45,5');
-    poly.setAttributeNS(null, 'fill', 'none');
-    poly.setAttributeNS(null, 'stroke', 'green');
-    
-    document.getElementById('svgBackground').appendChild(poly);
+    $('#start section').css({ height: startHeight + 'px' });
+    $('#start section div').css({ 
+        position: 'relative', 
+        top:($(window).height()/3) + 'px' 
+    });
+}
+
+/*
+ * Redimensionnement de l'accueil en cas de changement de la taille d'écran
+ */
+function applyResize() {
+    $(window).on('resize', function() { 
+        displayStart();
+    }); 
 }
 
 /*
@@ -51,30 +55,4 @@ function modMailTo() {
 
         $(this).attr('href', 'mailto:' + mail);
     });
-}
-
-/*
- * Dimensionnement de l'acceuil
- */
-function displayStart() {
-    var startHeight = $(window).height() - $('header nav').height();
-    
-    $('#start section').css({ height: startHeight + 'px' });
-    $('#start section div').css({ 
-        position: 'relative', 
-        top:($(window).height()/3) + 'px' 
-    });
-    $('#svgBackground').css({  
-        top: $('header nav').height() + 'px', 
-        bottom: startHeight + 'px',
-        width: $(window).width() + 'px',
-        heigth: startHeight
-    });
-}
-
-/*
- * Redimensionnement de l'accueil en cas de changement de la taille d'écran
- */
-function applyResize() {
-    $(window).on('resize', function() {  displayStart()	}); 
 }
