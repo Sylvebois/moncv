2ff3e62c4ef564b3106fe8b8129f82c7f2918a32
Sylvebois <thomasghaye@hotmail.com>
2016-02-22 15:18:29 +0100

    Création de la fenêtre modale + gestion des boutons de cette fenêtre + petite mise en scène de la présentation

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
-// Scrollspy fluide
 $(function () {  
-    checkModal();
     displayStart();
     applyResize();
     scrollSpy();
     modMailTo();
+    checkModal();
 });
 
 
@@ -64,12 +63,83 @@ function modMailTo() {
 
 //Vérifie si la fenêtre a déjà été ouverte
 function checkModal() {
-//    $(window).load(function(){
-//        $('#modalForm').modal('show');
-//    });
+    var cookies = document.cookie.split(';');
+
+    if(cookies.indexOf('formViewed=true') < 0) {
+        $(document).ready(function () {
+            //Mise en scène
+            $('header').hide();
+            $('#start section div').hide();
+            
+            //Chargement de la fenêtre
+            $('.modal-dialog').load('./includes/form_accueil.html');
+            $('#modalForm').modal('show');
+        }); 
+    }
+    
+    $('footer a').click(function() {
+        $('.modal-dialog').load('./includes/form_accueil.html');
+    });
+    
+    manageModal();
 }
 
-//Gestion des événements dans la fenêtre
+//Gestion des boutons dans la fenêtre
 function manageModal() {
+    //Début
+    $('#modalForm').on('click','#formStart',function(){
+        loadForm('form1');
+    });
+   
+   //Précédent
+    $('#modalForm').on('click','#form1 #formPrev',function(){
+        loadForm('form_accueil');
+    });
+    $('#modalForm').on('click','#form2 #formPrev',function(){
+        loadForm('form1')
+    });  
+    $('#modalForm').on('click','#form3 #formPrev',function(){
+        loadForm('form2')
+    });  
+ 
+    //Suivant
+    $('#modalForm').on('click','#form1 #formNext',function(){
+        loadForm('form2')
+    });
+    $('#modalForm').on('click','#form2 #formNext',function(){
+        loadForm('form3')
+    });
+    $('#modalForm').on('click','#form3 #formNext',function(){
+        loadForm('form_fin');
+        setTimeout(showResult(), 2000);
+   });
+   
+   //Fin
+    $('#modalForm').on('click','#formEnd',function(){
+       $('.modal-dialog').empty();
+       $('#modalForm').modal('hide');
+       document.cookie="formViewed=true"; 
+    });    
     
+    //Mise en scène
+   $('#modalForm').on('hidden.bs.modal', function(){
+        $('header').fadeIn('slow', function(){
+            $('#start section div').fadeIn(3000);     
+        });  
+   });
+}
+
+function showResult() {
+    $('.modal-header h2:first-of-type').addClass('hidden');
+    $('.modal-header h2:last-of-type').removeClass('hidden');
+    
+    $('.modal-body p:first-of-type').addClass('hidden');
+    $('.modal-body p:last-of-type').removeClass('hidden');
+    
+    $('#formEnd').removeClass('disabled');
+}
+
+function loadForm(nom) {
+    $('.modal-dialog').empty();
+    $('.modal-dialog').load('./includes/'+nom+'.html');    
 }
